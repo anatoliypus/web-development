b456d0ed878e2ba005874b473b5c999b574677b9
Анатолий Пушкарев <anatoliypuss5@gmail.com>
2020-04-22 15:39:41 +0300

    Structure corrected

--- lw6/php/writeMeTemplate.php
+++
@@ -1,4 +0,0 @@
-<form action="http://localhost:8080/php/showPerson.php" method="POST">
-  <input type="text" class="small-input" placeholder="Your email" name="email">
-  <input type="submit" value="Write back!" class="message-submit">
-</form>
