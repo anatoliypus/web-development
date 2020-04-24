4f680a5740613843f821ab18b9af0e6635dcd455
Анатолий Пушкарев <anatoliypuss5@gmail.com>
2020-04-24 22:36:08 +0300

    Validation added

--- lw6/src/pages/saveFeedbackPage.php
+++ lw6/src/pages/saveFeedbackPage.php
@@ -17,21 +17,34 @@
           $message = getPostParameter("message");
       }
 
-      if ($email != null) 
+      $cond = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+      if ($cond and $name != null and !ctype_digit($name[0])) 
       {
-        $fp = fopen("../src/data/".mb_strtolower($email).".txt", 'w');
-        $str = $name == null ? 'not stated' : $name;
-        fwrite($fp, "Name: ".$str."\r\n");
-        $str = $email == null ? 'not stated' : $email;
-        fwrite($fp, "Email: ".$str."\r\n");
-        $str = $subject == null ? 'not stated' : $subject;
-        fwrite($fp, "Subject: ".$str."\r\n");
-        $str = $message == null ? 'not stated' : $message;
-        fwrite($fp, "Message: ".$str."\r\n");
-        renderTemplate("main.php", ["scroll" => TRUE, "success" => TRUE]);
+          $fp = fopen("../src/data/".mb_strtolower($email).".txt", 'w');
+          $str = $name;
+          fwrite($fp, "Name: ".$str."\r\n");
+          $str = $email == null ? 'not stated' : $email;
+          fwrite($fp, "Email: ".$str."\r\n");
+          $str = $subject == null ? 'not stated' : $subject;
+          fwrite($fp, "Subject: ".$str."\r\n");
+          $str = $message == null ? 'not stated' : $message;
+          fwrite($fp, "Message: ".$str."\r\n");
+          renderTemplate("main.php", ["scroll" => TRUE, "success" => TRUE]);
       } else 
       {
-        renderTemplate("main.php", ["error_msg" => "Wrong email!", "scroll" => TRUE]);
+          if ($email == null) 
+          {
+              renderTemplate("main.php", ["error_msg" => "Type email!", "scroll" => TRUE]);
+          } else if ($name == null) {
+              renderTemplate("main.php", ["error_msg" => "Type name!", "scroll" => TRUE]);
+          } else if (ctype_digit($name[0])) 
+          {
+              renderTemplate("main.php", ["error_msg" => "Wrong name!", "scroll" => TRUE]);
+          } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+          {
+              renderTemplate("main.php", ["error_msg" => "Wrong email!", "scroll" => TRUE]);
+          }
       }
     }
     
